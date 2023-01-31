726221b3b216a3a50e935a3ea1247c9593ddc8ec
patrisfs <patrickfreitas6020@gmail.com>
2023-01-31 09:25:49 -0300

    componente de loading e layout dos project cards

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,15 +1,47 @@
 import { useLocation } from "react-router-dom"
+import { useState, useEffect } from "react"
+
 import Message from "../Layout/Message"
 import Container from "../Layout/Container"
+import Loading from "../Layout/Loading"
 import LinkButton from "../Layout/LinkButton"
+import ProjectCard from "../Project/ProjectCard"
+
 import styles from "./Projects.module.css"
 
+
+
 function Projects(){
+
+  const [projects, setprojects] = useState([])
+  const [removeLoading, setRemoveLoading] =useState(false)
+
+
   const location = useLocation()
   let message =''
   if(location.state){
     message = location.state.message
   }
+
+  useEffect(()=>{
+    setTimeout(() => {
+      fetch('http://localhost:5000/projects',{
+        method:'GET',
+        headers:{
+          'content-type': 'application/json',
+        },
+      })
+          .then((resp)=> resp.json())
+          .then((data)=>{
+            setprojects(data)
+            setRemoveLoading(true)
+            console.log(data)
+          })
+          .catch((err)=>console.log(err))
+    },200);
+  },[])
+
+
 
     return(
         <div className={styles.project_container}>
@@ -19,7 +51,15 @@ function Projects(){
          </div>
           {message && <Message type="sucess" msg={message}/>}
           <Container customClass="start">
-            <p>Projetos...</p>
+            {projects.length > 0 &&
+              projects.map((project) => <ProjectCard 
+              name={project.name}
+              id={project.id}
+              budget={project.budget}
+              category={project?.category?.name}
+              key={project.category.id}  
+              />)}
+              {!removeLoading && <Loading/>}
           </Container>
         </div>
     )
