c5658f7d66229ee6ba549bde49f88777a00ae069
patrisfs <patrickfreitas6020@gmail.com>
2023-02-02 09:50:24 -0300

    adicionando links ao footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,13 +1,14 @@
 import styles from './Footer.module.css'
-import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import {FaGithub, FaEnvelope, FaLinkedin} from 'react-icons/fa'
+
 function Footer(){
     return(
         <footer className={styles.footer}>
-            <ul className={styles.social_list}>
-                <li><FaFacebook/></li>
-                <li><FaInstagram/></li>
-                <li><FaLinkedin/></li>
-            </ul>
+        <ul className={styles.social_list}>
+            <li><a href='https://github.com/patrisfs' target='_blank' rel='noopener noreferrer'><FaGithub /></a></li>
+            <li><a href='mailto:patrick.freitas@engenharia.ufjf.br' target='_blank' rel='noopener noreferrer'><FaEnvelope /></a></li>
+            <li><a href='https://www.linkedin.com/in/patrick-freitas-5bb062194/' target='_blank' rel='noopener noreferrer'><FaLinkedin /></a></li>
+        </ul>
             <p className={styles.copy_right}>Compilado por patrick Freitas, 2023</p>
         </footer>
     )
